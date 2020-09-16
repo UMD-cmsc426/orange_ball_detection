81402b3502b6572db6885c2c24893ee71204b4aa
da_shen <dashen@unomaha.edu>
2020-09-16 17:51:44 -0400

    setup LINUX virtual env. added vectoried single gaussian

--- gaussian.py
+++ gaussian.py
@@ -3,6 +3,7 @@ import cv2
 import os
 import numpy as np
 import math
+import scipy
 
 # Define a function that generates a RGB image's mean value and covariance.
 # Output shape: mean-（3x1） covariacne-(3x3)
@@ -19,33 +20,59 @@ def cal_mean_cov(img):
     cov = cov/(l*w)
     return mean,cov
 
+
+# Define a vectorized function that generates a RGB image's mean value and covariance.
+# input shape: X:(num of pixels, 3)
+# Output shape: mean-（3,） covariacne-(3x3)
+def cal_mean_cov_vectorized(X):
+    N, D = X.shape
+    mean = X.mean(axis = 0) # compute mean
+    cov = np.matmul((X - mean).T, (X - mean))/(N-1) # compute covariance
+    return mean, cov
+
+
+
 def single_gussian(input_dir,output_dir, tau):
     for img_name in os.listdir(input_dir):
         img = cv2.imread(os.path.join(input_dir, img_name))
-        mean, cov = cal_mean_cov(img)
-        # creat a mask to indicate the position of ball
-        mask = np.zeros((img.shape[:-1]))
-        # print("cov: \n", cov)
+        l, w, h = img.shape # original shape of 2D image
+        X = img.transpose(2,0,1).reshape(3,-1).T # reshape to num of rows = num of pixels, num of column = 3 (RGB)
+        N, D = X.shape
+        #img = X.reshape(l, w, -1) # reshape back to 2d image
+    
+        # vectorized algorithm
+        mean, cov = cal_mean_cov_vectorized(X)    
+    
+        # calculate likelihood using gaussian distribution
+        # each pixel is row of X
+        constant_in_likelihood = 1/(math.sqrt(((2*math.pi)**3)* np.linalg.det(cov))) 
+        sigma_inv = np.linalg.inv(cov)
+        X2 = X-mean
+        exponent = (-0.5)*(np.dot(X2, sigma_inv) * X2).sum(1) 
+        likelihood = constant_in_likelihood * np.exp(exponent)
+    
+        # posterior
         prior = 0.5
-        likelihood_list = []
-        for width in range(len(img[:, 0, 0])):
-            for length in range(len(img[0, :, 0])):
-                curr_pixel = np.asmatrix([[img[width][length][0]], [img[width][length][1]], [img[width][length][2]]])
-                likelihood = 1 / (math.sqrt(((2 * math.pi) ** 3) * np.linalg.det(cov))) * math.exp(
-                    (-0.5) * (curr_pixel - mean).T @ np.linalg.inv(cov) @ (curr_pixel - mean))
-                # print("likelihood", likelihood)
-                likelihood_list.append(likelihood)
-                if (likelihood * prior < tau):
-                    mask[width][length] = 1
-        # stack mask to three channels  inorder to produce masks
-        three_d_mask = np.stack((mask, mask, mask), axis=2)
+        posterior = prior* likelihood 
+    
+        # mask
+        mask = posterior
+        mask[mask < tau] = -1 # temperarily mark as -1 to prevent being marked as 0 later
+        mask[mask != -1] = 0
+        mask[mask == -1] = 1
+        mask = mask.reshape(l, w) # reshape back to 2D image
+        # print("end vectorized algorithm", datetime.now().strftime("%H:%M:%S"))
+
+        ##  show mask    
+        three_d_mask = np.stack((mask,mask,mask),axis=2)
         masked_img = np.multiply(three_d_mask, img)
-        image_name = os.path.join(output_dir, "masked_" + str(img_name))
-        # store image to output directory
-        cv2.imwrite(image_name, masked_img)
-        cv2.waitKey()
+        image_name = os.path.join(output_dir,"masked_"+ str(img_name))
+        cv2.imshow(image_name, masked_img)
+        cv2.waitKey(0)
         print("Finish Generating mask for image ", str(img_name))
     print("Complete Process All Images")
+
+
 
 if __name__ == "__main__":
     # load data
@@ -54,4 +81,6 @@ if __name__ == "__main__":
     output_dir = "single_gaussian_result"
     # User defined threshold
     tau = 0.00000000000000001
+    prior = 0.5
     single_gussian(input_dir,output_dir,tau)
+
